db642f55dd92fbcc30d121efa0c75944c8b6fc3c
ReyTabasan <reytabasan123@gmail.com>
2025-11-04 06:46:39 -0500

    updated

--- api/api_share.php
+++ api/api_share.php
@@ -2,7 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require '../vendor/autoload.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $document_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
     if (!$document_id) {
